fec920a375d99215e85c3f36d62060cf9ba81c9f
Mario Probst <mario.probst2@gmail.com>
2024-06-16 12:49:02 +0200

    installed firabase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule
   ],
   providers: [],
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAP1UFfzpuS7s1JC_SybWw6gelKTN8aqHg",
+        authDomain: "mstart-8a749.firebaseapp.com",
+        projectId: "mstart-8a749",
+        storageBucket: "mstart-8a749.appspot.com",
+        messagingSenderId: "551792260354",
+        appId: "1:551792260354:web:60dee4c83448c33fbdbe1b",
+        measurementId: "G-C4PL7E3SL4"
+    }
+  };
